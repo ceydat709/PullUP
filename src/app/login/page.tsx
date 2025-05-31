1a6859a18de28a54f6b38647db174c633c83c05c
ceydat709 <ceydatopcu5334@gmail.cpm>
2025-05-30 20:56:36 -0400

    [feat]: Added logo to the sign in page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,27 @@
 'use client';
 
 import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
 import AuthForm from '../../components/AuthForm';
 
 export default function LoginPage() {
   return (
-    <main className="min-h-screen flex items-center justify-center bg-white px-6">
-      <AuthForm />
+    <main className="min-h-screen bg-white px-6">
+      <div className="absolute top-4 left-4">
+        <Link href="/">
+          <Image
+            src="/media/Logo.png"
+            alt="PullUp Logo"
+            width={220} 
+            height={140}
+            className="cursor-pointer"
+          />
+        </Link>
+      </div>
+      <div className="flex items-center justify-center min-h-screen">
+        <AuthForm />
+      </div>
     </main>
   );
 }
